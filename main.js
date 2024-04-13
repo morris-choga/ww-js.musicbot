5ad36e0642eeacd504957082cf89d3dd17e82d59
morrischoga <mchoga@outlook.com>
2024-04-13 08:35:29 +0200

    initial bot commit

--- main.js
+++ main.js
@@ -1,302 +1,230 @@
 const qrcode = require('qrcode-terminal');
-const { Client,LocalAuth ,MessageMedia, Buttons } = require('whatsapp-web.js');
-
-
-const client = new Client({
-    authStrategy: new LocalAuth(),
-     puppeteer: {
-           headless: true,
-           args: ['--no-sandbox']
-           }
-});
-process.on('SIGINT', async () => {
-    await this.client.destroy();
-    process.exit(0);
-});
-
-client.on('qr', (qr) => {
-    qrcode.generate(qr, { small: true });
-});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-const fs = require('fs');
+const { sendSong , sendLyrics, sendSongInfo, searchSong} = require("./messenger");
+const { Client,LocalAuth ,MessageMedia, LinkingMethod } = require('whatsapp-web.js');
 
 
 
-let apiKey = "patg3nVCYWdoRthJn.56198a4363e0982055386462c75e70566e51bc2b4bac7cd605b6996a87b51521";
-let airtableUrl = "https://api.airtable.com/v0/appAcgdXpcBoOwP5X/tblk48SN08xOlGQz9"
-let apiUrl = "http://127.0.0.1:5000";
-
-let requestOptions = {
-    method: 'POST',
-    headers: {"Content-Type": "application/json"},
-    body: {},
-    redirect: 'follow'
-};
-
-let addSong = {
-    "fields": {"#songs":10}
-}
-
-userInfo = {
-    "records": [{"fields":{
-        "userID": "","userName": "","userCountry": "","#songs": 1
-    }
-}]}
-
-async function fetchCountry(num){
-    let result = await fetch(`https://lookups.twilio.com/v2/PhoneNumbers/+${num}`, {
-        headers: {
-            'Authorization': "Basic QUMwYTEyNDRhYzA4Y2Q3Nzc1ZTFhZjhmZjg2ODk2OTNiYjpmMjNhYjU1NTJkM2NiNjUxZjc2ZDhiODhhYjM4YmU4Yg==",
-            'Content-Type': 'application/json',
-        },
-    }).then((response)=>{
-        let body = response.json()
-
-        return body}).then((data)=>{
-            let country = data.country_code
-        return country
-    }).catch(error=>{
-        console.log(`An error occurred while fetching country: ${error}`)
-    })
-    return result
-}
 
 
-async  function songIncrement(id){
+
+class Bot{
+    messageCount = 0;
 
 
-    let result = await fetch(`${airtableUrl}/${id}`, {
-        method: 'PATCH',
-        headers: {
-            'Authorization': `Bearer ${apiKey}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(addSong)
-    })
+    constructor(sessionName,range) {
 
-}
+        let reInitializeCount = 0
+        this.client = new Client({
 
-async function fetchUsers(){
+            // linkingMethod:  new LinkingMethod({
+            //     phone: {
+            //         number: "+639514176425"
+            //     }
+            // }),
+            authStrategy: new LocalAuth({
+                dataPath: "/usr/src/app/sessions",
+                // dataPath: "C:\\Users\\Mchog\\WebstormProjects\\ww-js.musicbot\\sessions",
+                clientId: `${sessionName}`
+            }),
+            puppeteer: {
+                headless: true,
+                args: [
+                    '--no-sandbox',
+                    // '--disable-setuid-sandbox',
+                    // '--single-process',
+                    // '--disable-gpu'
+                ]
+            },
+            // webVersion: '2.2409.2',
+            // webVersionCache: {
+            //     type: 'remote',
+            //     // remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
+            //     remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2409.2.html'
+            // }
 
-    let result = await fetch(airtableUrl,{
-        headers: {
-            'Authorization': `Bearer ${apiKey}`,
-            'Content-Type': 'application/json',
-        },
-    }).then((response)=>{
-        let body = response.json()
+        });
+        process.on('SIGINT', async () => {
+            await this.client.destroy();
+            process.exit(0);
+        });
 
-        return body
-    }).then((response)=>{
+        this.client.on('qr', (qr) => {
+            qrcode.generate(qr, { small: true });
+        });
 
-        let userInfo = {}
-        let users = response.records
+        this.client.on('code', (code) => {
+            console.log('CODE RECEIVED', code);
+
+        });
 
 
-        Object.keys(users).forEach((key) => {
+        this.client.on('ready', () => {
+            console.log(`bot ${sessionName} ready!`);
+        });
 
-            // userInfo[users[key].fields.userID] = users[key].fields["#songs"]
-            userInfo[users[key].fields.userID] = [users[key].id,users[key].fields["#songs"]]
+
+        this.client.on('message', async (message) => {
+            let userId = (await message.id.participant);
+            let song_group = "120363223962652835"
+            let test_group = "120363243170575745"
+            let lyrics_group =  "120363244367417149"
+            let chat_id = (await message.getChat()).id.user
+            let message_body = message.body.toLocaleLowerCase()
+            let groupParticipantsNumber = (await message.getChat()).isGroup ? (await message.getChat()).participants.length : 0
+
+            if (message_body.startsWith("message_count")){
+                console.log(`Bot ${sessionName} has ${this.getMessageCount()} messages`)
+
+            }
+
+            if((message_body.includes("https://")) && !message_body.includes("request") && (chat_id === test_group || chat_id === lyrics_group || chat_id === song_group)){
+
+                setTimeout(async ()=>{
+
+                    try {
+                        await message.delete(true)
+
+                    } catch (error) {
+                        console.log(`Error deleting message ${error}`)
+
+                    }
+
+
+                }, 10000);
+
+
+            }
+
+            if ((message_body.startsWith("!song") || message_body.startsWith("!lyrics")) && message.body.length > 6 && !(await message.getChat()).isGroup){
+
+                await message.reply("For now the bot can only work in a group chat. Please add me in a group to  request for songs...")
+                this.messageCount++;
+
+            }
+
+            if((await message.getChat()).isGroup && (chat_id !== song_group && chat_id !== lyrics_group && chat_id !== test_group ) && (message_body.startsWith("!song") || message_body.startsWith("!lyrics"))){
+
+                if (groupParticipantsNumber < 11) {
+                    setTimeout(async () => {
+                        await message.reply(`The music bot only works in a group with at least 10 participants. Please add ${11 - (await message.getChat()).participants.length} more people to the group`)
+                        this.messageCount++;
+                    }, 5000);
+                }
+
+                else {
+
+                    await message.reply("Join the group to request for songs \n\nhttps://chat.whatsapp.com/F1l3b5zU8N652cm0gmUuUS")
+                    this.messageCount++;
+
+
+                }
+
+            }
+
+
+
+
+            if(userId === undefined ? false : range.includes(parseInt(userId.substring(0, userId.indexOf('@')).charAt(userId.substring(0, userId.indexOf('@')).length - 1)))){
+
+
+
+                let options = ["1","2","3"]
+                let isGroup = (await message.getChat()).isGroup
+
+                if (message_body.startsWith("id")&& isGroup){
+                    console.log(message)
+                    console.log(chat_id)
+                }
+
+
+
+
+
+                else if((message_body.startsWith("!menu") || message_body.startsWith("!help")) && (chat_id === song_group || chat_id === lyrics_group || chat_id === test_group)){
+                    await message.reply("*Bot commands*\n\n🤖*!song* (eg !song rihanna diamonds)\n🤖*!lyrics* (eg !lyrics Maroon 5 sugar)\n🤖*!song-info* (eg !song-info eminem not afraid. Get information about a song. )\n\nNB: !song-info can be used to verify if a song exists to avoid requesting and downloading wrong song")
+                    this.messageCount++;
+                }
+
+
+                else if (message_body.startsWith("!lyrics ") && message.body.length > 8 && (chat_id === lyrics_group || chat_id === test_group)){
+                    await sendLyrics(message,this.client)
+                    this.messageCount++;
+                }
+                else if (message_body.startsWith("!lyrics ") && message.body.length > 8 ){
+                    await message.reply("Join the group to request for lyrics \n\nhttps://chat.whatsapp.com/DGeFgy7DRODIIgF68mojTP")
+                    this.messageCount++;
+                }
+
+                else if ((message_body.startsWith("!song-info ") || message_body.startsWith("!song_info ")) && (message.body.length > 11) && (chat_id === lyrics_group || chat_id === song_group)) {
+                    await sendSongInfo(message,this.client)
+                    this.messageCount++;
+
+                }
+
+                else if (message_body.startsWith("!song ") && message.body.length > 6 && isGroup) {
+
+                    if (chat_id === lyrics_group) {
+
+                        await message.reply("Join the group to request for songs \n\nhttps://chat.whatsapp.com/F1l3b5zU8N652cm0gmUuUS")
+                        this.messageCount++;
+                        // await message.reply("The bot is undergoing maintenance. Contact the admin to offer support for the project 😊")
+
+                    }
+
+
+
+
+                }
+
+
+
+                else if (message_body.startsWith("!album ") && message.body.length > 7 && isGroup) {
+
+
+
+                    await message.reply("Album request is still in development...")
+                    this.messageCount++;
+
+
+                }
+
+            }
+
+
+
 
         })
 
-        return userInfo
+        this.client.on("disconnected",(reason)=>{
+            console.log(`Client has been disconnected Morris because of ${reason} `);
 
-    }).catch(error=>{
-        console.log(`An error occurred while fetching https://api.airtable.com: ${error}`)
-    });
-
-    return result
-
-}
-
-async function addUser(){
-
-    await fetch(airtableUrl, {
-        method: 'POST',
-        headers: {
-            "Authorization": `Bearer ${apiKey}`,
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(userInfo)
-    }).catch(error=>{
-        console.log(`An error occurred while addingUser https://api.airtable.com: ${error}`)
-    })
-}
-
-
-client.on('message', async (message) => {
-
-
-
-    let groupParticipantsNumber = (await message.getChat()).isGroup ? (await message.getChat()).participants.length : 0
-    let isGroup = (await message.getChat()).isGroup
-
-
-    if (message.body.toLocaleLowerCase().startsWith("!song ") && message.body.length > 6 && isGroup) {
-
-
-        if ((await message.getChat()).id.user === "120363213455576189") {
-
-            let userID = (await message.id.participant).substring(0,(await message.id.participant).indexOf('@'))
-            let userName = await message._data.notifyName
-            let userCountry = await fetchCountry(userID)
-            let registeredUsers =await fetchUsers()
-
-
-
-
-            Object.keys(registeredUsers).includes(userID) ? await (async function () {
-
-                if (registeredUsers[userID][1] < 10) {
-                    console.log("Sending song ")
-                    addSong.fields["#songs"] = registeredUsers[userID][1] + 1
-                    songIncrement(registeredUsers[userID][0])
-
-                    let songPath = await fetch(apiUrl, requestOptions)
-                        .then((response) => {
-                            if (response.ok) {
-                                return response.text()
-                            }
-                            return "Error"
-                        }).then((data) => {
-                            let response = data
-                            let apiResponse = response.replace("api", "app")
-                            return apiResponse
-                        }).catch(error => console.log('an error has occurred while fetching https://127.0.0.1:5000 ', error))
-                } else {
-                    message.reply("You have exceeded your daily limit...")
-                }
-
-            })():(function () {
-                userInfo.records[0].fields.userID = userID
-                userInfo.records[0].fields.userName = userName
-                userInfo.records[0].fields.userCountry = userCountry
-                addUser()
-            })()
-
-            requestOptions.body = JSON.stringify({"key": message.body})
-
-
-
-            if (typeof songPath !== "undefined" && songPath !== "Error") {
-                let song = MessageMedia.fromFilePath(songPath)
-
-                try {
-                    await message.reply(song)
-                } catch (e) {
-                    console.log(`An error has occurred while sending media: ${e}`)
-                }
-
-                fs.unlink(songPath, (err) => {
-                    if (err) {
-                        console.error(`Error deleting file: ${err.message}`);
-                    } else {
-                        console.log(`${message.body.toLocaleLowerCase()} sent`);
-                    }
-                });
-
-            } else if (!isGroup) {
-                await message.reply("For now the bot can only work in a group chat. Please add me in a group to  request for songs...")
+            if(reInitializeCount < 15 && reason === 'NAVIGATION') {
+                reInitializeCount++;
+                this.client.initialize();
+                return;
             }
-        }
-
-
-        else if (groupParticipantsNumber < 11) {
-            setTimeout(async () => {
-                await message.reply(`The music bot only works in a group with at least 10 participants. Please add ${11 - (await message.getChat()).participants.length} more people to the group`)
-            }, 5000);
-              } else if (groupParticipantsNumber >= 11) {
-            setTimeout(async () => {
-                await message.reply("Join the community...")
-            }, 5000);
-
-        }
-
+        });
     }
 
-    else if (message.body.toLocaleLowerCase().startsWith("!song ") && message.body.length > 6 && !isGroup){
-        setTimeout(async () => {
-            await message.reply("For now the bot can only work in a group chat. Please add me in a group to  request for songs...")
-        }, 5000);
-
+    getMessageCount(){
+        return this.messageCount
+    }
+    initialize() {
+        // Initialize your client here if necessary
+        this.client.initialize();
     }
 
-    else if (message.body.toLocaleLowerCase().startsWith("!album ") && message.body.length > 7 && isGroup) {
-        setTimeout(async () => {
-            await message.reply("Album request is still in development...")
-        }, 5000);
-
-    }
-
-})
 
 
 
 
 
+}
 
-//     if(message.body.toLocaleLowerCase().startsWith("!song ") && message.body.length > 6 && isGroup  && (await message.getChat()).id.user === "120363213455576189"){
-//
-//         requestOptions.body = JSON.stringify({"key": message.body})
-//         let songPath = await fetch(apiUrl, requestOptions)
-//             .then((response) => {
-//                 if(response.ok){
-//                     return response.text()
-//                 }
-//                 return "Error"
-//             })
-//             .then((data)=>{
-//                 let response = data
-//                 let apiResponse = response.replace("api","app")
-//                 return apiResponse
-//             })
-//             .catch(error => console.log('an error has occurred while fetching https://api:5000 ', error))
-//
-//
-//
-//         if (typeof songPath !== "undefined" && songPath !=="Error"){
-//             let song = MessageMedia.fromFilePath(songPath)
-//
-//             try{
-//                 await message.reply(song)}
-//             catch (e){
-//                 console.log(`An error has occurred while sending media: ${e}`)
-//             }
-//
-//             fs.unlink(songPath, (err) => {
-//                 if (err) {
-//                     console.error(`Error deleting file: ${err.message}`);
-//                 } else {
-//                     console.log(`${message.body.toLocaleLowerCase()} sent`);
-//                 }
-//             });
-//
-//         }
-//     }
-//
-//     else if (message.body.toLocaleLowerCase().startsWith("!album ") && message.body.length > 7 && isGroup){
-//         await message.reply("Album request is still in development...")
-//     }
-//
-//     else if (message.body.toLocaleLowerCase().startsWith("!song ") && message.body.length > 6 && isGroup  && groupParticipantsNumber < 11){
-//         await message.reply(`The music bot only works in a group with at least 10 participants. Please add ${11 - (await message.getChat()).participants.length} more people to the group`)
-//     }
-//     else if (message.body.toLocaleLowerCase().startsWith("!song ") && message.body.length > 6 && !isGroup){
-//         await message.reply("For now the bot can only work in a group chat. Please add me in a group to  request for songs...")
-//     }
-//
-//     if(message.body.toLocaleLowerCase().startsWith("!song ") && message.body.length > 6 && isGroup  && groupParticipantsNumber >= 11){
-//         await message.reply("Join the community...")
-//     }
-//
-//
-//
-// });
+// const bot1 = new Bot("4789",[0,1,2,3,4]);
+const bot2 = new Bot("9552",[5,6,7,8,9]);
 
-client.initialize();
+// bot1.initialize();
+bot2.initialize();
+
 
---
+++ messenger.js
@@ -0,0 +1,114 @@
+const {MessageMedia} = require("whatsapp-web.js");
+
+const fs = require("fs");
+
+let apiUrl = "http://127.0.0.1:5000";
+// let apiUrl = "http://api:5000";
+let requestOptions = {
+    method: 'POST',
+    headers: {"Content-Type": "application/json"},
+    body: {},
+    redirect: 'follow'
+};
+
+
+const sendLyrics =  async (message,client) => {
+    requestOptions.body = JSON.stringify({"key": message.body.substring(8)})
+    let lyrics = await fetch(`${apiUrl}/lyrics`, requestOptions)
+        .then((response) => {
+
+
+            if (response.ok) {
+                return response.json()
+            }
+            return "Error"
+        }).then((data) => {
+            return data
+        }).catch(error => console.log('an error has occurred while fetching https://api:5000/lyrics ', error))
+
+
+    if (typeof lyrics === "object" && !Object.keys(lyrics).length == 0){
+        let picture = await MessageMedia.fromUrl(lyrics["album_art"], { unsafeMime: true })
+
+
+        setTimeout(async ()=>{
+
+            try {
+                await client.sendMessage(message._data.from,picture,{caption: lyrics["lyrics"],quotedMessageId:message.id._serialized})
+            } catch (error) {
+                console.log(`Error sending message ${error}`)
+
+            }
+
+        }, 10000);
+
+
+
+
+    }
+    else {
+        setTimeout(async ()=>{
+
+            try {
+                await message.reply("oops! lyrics for this song are unavailable\nuse !menu for help")
+
+            } catch (error) {
+                console.log(`Error sending message ${error}`)
+
+            }
+
+        }, 10000);
+
+
+
+        console.log("An error has occurred while searching lyrics: No object was received or the object was empty")
+    }
+}
+
+const sendSongInfo =  async (message,client) => {
+
+    requestOptions.body = JSON.stringify({"key": message.body.substring(11)})
+    let songInfo = await fetch(`${apiUrl}/getsonginfo`, requestOptions)
+        .then((response) => {
+
+
+            if (response.ok) {
+                return response.json()
+            }
+            return "Error"
+        }).then((data) => {
+            return data
+        }).catch(error => console.log('an error has occurred while fetching https://api:5000/getsonginfo', error))
+
+
+    if (typeof songInfo === "object" && !Object.keys(songInfo).length === 0){
+        let picture = await MessageMedia.fromUrl(songInfo["album_art"], { unsafeMime: true })
+
+
+        setTimeout(async ()=>{
+            try {
+                await client.sendMessage(message._data.from,picture,{caption: `*Title: ${songInfo.title}*\n*Artist: ${songInfo.artist}*\n*Album: ${songInfo.album}*\n*Year: ${songInfo.year}*`,quotedMessageId:message.id._serialized})
+
+            } catch (error) {
+                console.log(`Error sending message ${error}`)
+            }
+
+        }, 10000);
+
+
+    }
+    else {
+        setTimeout(async ()=>{
+
+            await message.reply("oops! info for this song are unavailable")
+            console.log("An error has occurred while searching song info: No object was received or the object was empty")
+        }, 10000);
+
+    }
+
+}
+
+
+
+
+module.exports = { sendLyrics, sendSongInfo};
